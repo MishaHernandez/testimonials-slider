d041fb15df69f48b4d4c2b2377b691ff9e6509d4
John <mijailhernandez@gmail.com>
2020-08-30 21:07:16 -0500

    avance v1.0

--- script.js
+++ script.js
@@ -1,34 +1,49 @@
-const btn = document.getElementById('btn')
-const image_1 = document.getElementById('image_1')
-// const image_2 = document.getElementById('image_2')
+const button = document.getElementById('btn')
+const image = document.getElementById('image')
+const comment = document.getElementById('comment')
+const name = document.getElementById('name')
+const position = document.getElementById('position')
 
 const data = [
     {
         picture: 'images/image-tanya.jpg',
-        comment: `I’ve been interested in coding for a while but never taken the jump, until now. 
+        comment: `" I’ve been interested in coding for a while but never taken the jump, until now. 
         I couldn’t recommend this course enough. I’m now in the job of my dreams and so 
-        excited about the future.`,
+        excited about the future. "`,
         name: 'Tanya Sinclair',
         position: 'UX Engineer'
     },
     {
         picture: 'images/image-john.jpg',
-        comment: `If you want to lay the best foundation possible I’d recommend taking this course. 
+        comment: `" If you want to lay the best foundation possible I’d recommend taking this course. 
         The depth the instructors go into is incredible. I now feel so confident about 
-        starting up as a professional developer.`,
+        starting up as a professional developer. "`,
         name: 'John Tarkpor',
         position: 'Junior Front-end Developer'
     }
 ]
 
-btn.addEventListener('click', (e)=> {
+showData = (index)=> {
+    image.setAttribute('src', data[index].picture)
+    comment.textContent = data[index].comment
+    name.textContent = data[index].name
+    position.textContent = data[index].position
+}
+
+let index = 0
+
+btn.addEventListener('click', (e)=> {    
     if (e.target.id === 'back') {
-        // console.log('Se encontró un boton izquierdo')
+        if (index > 0) index--
+        else index = data.length - 1
         
-    } else if (e.target.id === 'next') {
-        // console.log('Se encontró un boton derecho')
-    } else {
-        console.log('No se encontó boton')
-    } 
+        showData(index)
+        
+    } else { // if (e.target.id === 'next')
+        if (index < data.length - 1) index++
+        else index = 0
+        
+        showData(index)
+    }
 })
 
